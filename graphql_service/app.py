00a31d9484a2a29375639d2c9bf8178af7485061
EzYoloFrodo <koroltchukjaroslav@gmail.com>
2020-05-16 16:35:45 +0300

    feat: add full graphql server support

--- graphql_service/app.py
+++ graphql_service/app.py
@@ -1,7 +1,9 @@
 # Imports
 from flask import Flask
-from pymongo import MongoClient
-from flask_sq
+import graphene
+from graphene_mongo import MongoengineObjectType
+from flask_graphql import GraphQLView
+from mongoengine import *
 import json
 
 
@@ -9,28 +11,67 @@ import json
 app = Flask(__name__)
 app.debug = True
 
-client = MongoClient("mongo", 27017, username="root", password="example")
-db = client["news"]
 
+connect("news", host="mongo", port=27017, username="root", password="example", authentication_source='admin')
 # Configs
 # TO-DO
+
 
 # Modules
 # TO-DO
 
-# Models
-# TO-DO
 
-# Schema Objects
-# TO-DO
+class PostsModel(Document):
+    meta = {'collection': 'posts'}
+    _id = ObjectIdField()
+    link = StringField()
+    title = StringField()
+    text = StringField()
+    author_name = StringField()
 
-# Routes
-# TO-DO
+
+class Post(MongoengineObjectType):
+    class Meta:
+        model = PostsModel
+
+
+class Query(graphene.ObjectType):
+    posts = graphene.List(Post)
+    post_title = graphene.List(Post, title=graphene.String())
+    post_n = graphene.List(Post, number=graphene.Int())
+
+    def resolve_posts(self, info):
+        return list(PostsModel.objects.all())
+
+    def resolve_post_title(self, info, title):
+        return list(PostsModel.objects.filter(title=title))
+
+    def resolve_post_n(self, info, number):
+        return list(PostsModel.objects.skip(number-1).order_by("_id").limit(1))
+
+
+schema = graphene.Schema(query=Query)
+
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True  # for having the GraphiQL interface
+    )
+)
 
 
 @app.route('/')
 def index():
     return '<p> Hello World</p>'
+
+
+@app.route("/test")
+def test_mongo():
+    a = PostsModel.objects.first()
+    print(a)
+    return a
 
 
 if __name__ == '__main__':
